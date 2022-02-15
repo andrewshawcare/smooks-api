667ab398b8e85eb6e867f58019d1aa49902702a3
Andrew Shaw Care <andrewshawcare@gmail.com>
2022-02-14 21:41:47 -0500

    Add exception handling; Add style changes

--- src/main/kotlin/com/andrewshawcare/smooks_api/PurchaseOrderDocumentController.kt
+++ src/main/kotlin/com/andrewshawcare/smooks_api/PurchaseOrderDocumentController.kt
@@ -1,5 +1,6 @@
 package com.andrewshawcare.smooks_api
 
+import org.milyn.SmooksException
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -9,10 +10,14 @@ import org.springframework.web.multipart.MultipartFile
 class PurchaseOrderDocumentController {
     @PostMapping("/purchase-order-document")
     fun bindPurchaseOrderDocument(@RequestParam("purchaseOrderDocument") purchaseOrderDocumentMultipartFile: MultipartFile): PurchaseOrderDocument {
-        return EdiDocumentService("smooks_resource_list.xml")
-            .bind(
-                inputStream = purchaseOrderDocumentMultipartFile.inputStream,
-                clazz = PurchaseOrderDocument::class.java
-            )
+        try {
+            return EdiDocumentService("smooks_resource_list.xml")
+                .bind(
+                    inputStream = purchaseOrderDocumentMultipartFile.inputStream,
+                    clazz = PurchaseOrderDocument::class.java
+                )
+        } catch (smooksException: SmooksException) {
+            throw smooksException.cause!!
+        }
     }
 }
